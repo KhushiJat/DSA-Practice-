f920c28851c881ff1991ca9592bf2549027b40b2
Khushi Jat <jatk14619@gmail.com>
2026-03-26 08:02:51 +0530

    Create 04_Rotate_Array.java

---
+++ Arrays/LeetCode_Array_Questions/04_Rotate_Array.java
@@ -0,0 +1,27 @@
+// Problem : 4. Rotate Array
+// Link : https://leetcode.com/problems/rotate-array/submissions/1959434065/?envType=problem-list-v2&envId=array
+// Difficulty : Medium
+// Approach : Reversal Algorithm (In-place Array Rotation Approach).
+
+// Time complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+  public void rotateArray(int[] nums, int k){
+    int n = nums.length;
+    k = k%n;  // for overflow codition
+
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
+  }
+  public void reverse(int[] nums, int left, int right){
+    while(left<right){
+      int temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+      right--;
+    }
+  }
+}
