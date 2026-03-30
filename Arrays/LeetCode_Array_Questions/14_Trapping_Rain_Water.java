2e4aa4124d115ba568c9ff62c88b9bae8c911243
Khushi Jat <jatk14619@gmail.com>
2026-03-30 08:57:10 +0530

    Create 14_Trapping_Rain_Water.java

---
+++ Arrays/LeetCode_Array_Questions/14_Trapping_Rain_Water.java
@@ -0,0 +1,35 @@
+// Problem : 14. Trapping Rain Water 
+// Link : https://leetcode.com/problems/trapping-rain-water/?envType=problem-list-v2&envId=array
+// Difficulty : Hard 
+
+// Approach : Two Pointer
+// Time Complexicty : O(n)
+// Space complexicty : O(1)
+
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    res += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    res += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return res;
+    }
+}
