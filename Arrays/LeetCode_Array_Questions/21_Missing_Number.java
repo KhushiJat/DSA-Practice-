74ec18f9a2f2694755b38da0164edcd4f10c7299
Khushi Jat <jatk14619@gmail.com>
2026-04-03 07:53:35 +0530

    Create 21_Missing_Number.java

---
+++ Arrays/LeetCode_Array_Questions/21_Missing_Number.java
@@ -0,0 +1,19 @@
+// Problem : 268. Missing Number
+// Link : https://leetcode.com/problems/missing-number/description/?envType=problem-list-v2&envId=array
+// Difficulty : Easy
+
+// Approach : XOR - optimal 
+// Time complexity : O(n)
+// Space complexity : O(1)
+
+class Solution {
+  public int missingNumber(int[] nums) {
+    int n = nums.length;
+    int xor = n;
+
+  for(int i=0; i<n; i++){
+    xor ^= i^ nums[i];
+  }
+  retrun xor;
+  }
+}
