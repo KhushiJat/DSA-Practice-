59417d16b26079a851fc846362fa1bec90433de9
Khushi Jat <jatk14619@gmail.com>
2026-04-11 07:37:09 +0530

    Update 10_Remove_DuplicatesFromSortedArray.java

--- Arrays/LeetCode_Array_Questions/10_Remove_DuplicatesFromSortedArray.java
+++ Arrays/LeetCode_Array_Questions/10_Remove_DuplicatesFromSortedArray.java
@@ -8,15 +8,25 @@
 
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // k represents the position where the next valid element will be placed
         int k = 0;
 
         for (int i = 0; i < nums.length; i++) {
+             // Condition explanation:
+            // 1. If k < 2 → we can always take first two elements
+            // 2. If current element is NOT equal to nums[k-2]
+            //    → ensures we allow at most 2 duplicates
             if (k < 2 || nums[i] != nums[k - 2]) {
+                // Place current element at position k
                 nums[k] = nums[i];
+                // Move k forward
                 k++;
             }
+            // Else case (implicitly skipped):
+            // If nums[i] == nums[k-2], we skip it
+            // because we already have 2 occurrences
         }
-
+         // k is the new length of the array with valid elements
         return k;
     }
 }
